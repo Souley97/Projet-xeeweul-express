841bd64771dfe97670ad5caea19b1f0fb7c73dd5
Souley97 <souleymane9700@gmail.com>
2024-07-16 16:29:18 +0000

    fix: test

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -60,106 +60,13 @@ class SubscriptionController extends Controller
 
     public function showAcceptedTransactions()
     {
-        $payments = Payment::where('status', 'ACCEPTED')->get();
+        $payments = Subscriptions::all();
 
         return view('subscription.payments.liste', compact('payments'));
     }
-    public function confirmation()
-    {
-        return view('subscription.confirmation');
-    }
-
-    public function handlePaymentNotification(Request $request)
-    {
-        // Vérifier si la notification contient l'ID de transaction
-        if (!$request->has('transaction_id')) {
-            // Retourner une réponse JSON indiquant que l'ID de transaction est manquant
-            return response()->json(['status' => 'error', 'message' => 'Transaction ID missing'], 400);
-        }
-
-        // Récupérer l'ID de transaction depuis la requête
-        $transaction_id = $request->input('transaction_id');
-
-        // URL de l'API CinetPay pour vérifier le statut du paiement
-        // $apiUrl = 'https://app-new.cinetpay.com/transactions/payments/';
-        $apiUrl = 'https://api.cinetpay.com/v2/?method=checkPayStatus';
 
 
-        // Envoyer une requête à l'API CinetPay pour vérifier le statut du paiement
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json', // Définir le type de contenu comme JSON
-            'apikey' => '15942973496654975ddaeab3.97350916', // Inclure l'API key pour l'authentification
-        ])->post($apiUrl, [
-                    'cpm_site_id' => '5866754', // Inclure l'ID du site pour l'authentification
-                    'cpm_trans_id' => $transaction_id, // Inclure l'ID de transaction à vérifier
-                ]);
 
-        // Vérifier si la requête à l'API a échoué
-        if (!$response->successful()) {
-            // Obtenir le corps de la réponse d'erreur
-            $errorBody = $response->body();
-            // Enregistrer un message d'erreur dans les logs
-            Log::error('Failed to fetch transaction from CinetPay API.', ['response' => $errorBody]);
-            // Retourner une réponse JSON indiquant l'échec de la récupération de la transaction
-            return response()->json(['status' => 'error', 'message' => 'Failed to fetch transaction', 'error' => $errorBody], 500);
-        }
-
-        // Décoder la réponse JSON de l'API
-        $response_body = $response->json();
-
-        // Vérifier si la transaction n'est pas acceptée (code != '00')
-
-
-        // Extraire les données de la transaction de la réponse
-        $transaction = $response_body['data'];
-
-        // Récupérer l'utilisateur associé à la transaction via l'email (ou une autre méthode d'identification)
-        $user = User::where('email', $transaction['customer_email'])->first();
-
-        // Vérifier si l'utilisateur n'a pas été trouvé
-        // if (!$user) {
-        //     // Retourner une réponse JSON indiquant que l'utilisateur n'a pas été trouvé
-        //     return response()->json(['status' => 'error', 'message' => 'User not found'], 404);
-        // }
-
-        // Récupérer ou créer un nouvel abonnement pour l'utilisateur
-        $subscription = Subscriptions::firstOrNew([
-            'user_id' => $user->id, // ID de l'utilisateur
-            'subscription_plan_id' => $transaction['plan_id'], // ID du plan d'abonnement (doit être fourni dans les métadonnées ou la transaction)
-        ]);
-
-        // Définir la date de début de l'abonnement comme maintenant
-        $subscription->start_date = now();
-        // Définir la date de fin de l'abonnement en ajoutant la durée du plan (doit être fourni dans les métadonnées ou la transaction)
-        $subscription->end_date = now()->addDays($transaction['plan_duration']);
-        // Définir le statut de l'abonnement comme "active"
-        $subscription->status = 'active';
-        // Sauvegarder l'abonnement dans la base de données
-        $subscription->save();
-
-        // Créer un tableau avec les informations de la transaction
-        $transaction_data = [
-            'created_at' => $transaction['created_at'],
-            'payment_date' => $transaction['payment_date'],
-            'business' => 'xeeweul-express',
-            'business_ID' => 5866754,
-            'operator' => $transaction['operator'] ?? 'N/A',
-            'transaction_id' => $transaction_id,
-            'operator_id' => $transaction['operator_id'] ?? 'N/A',
-            'phone' => $transaction['phone_number'] ?? 'N/A',
-            'paid_amount' => $transaction['amount'],
-            'currency' => 'XOF',
-            'sync' => 'Y',
-            'status' => $transaction['status'],
-            'commentaire' => $transaction['comment'] ?? 'N/A',
-        ];
-
-        // Sauvegarder les informations de la transaction dans un fichier JSON
-        Storage::put("transactions/{$transaction_id}.json", json_encode($transaction_data, JSON_PRETTY_PRINT));
-
-        // Retourner une réponse JSON indiquant que la transaction a été traitée avec succès
-        return response()->json(['status' => 'success', 'message' => 'Transaction processed successfully']);
-    }
 
 
 }
--- resources/views/subscription/payments/liste.blade.php
+++ resources/views/subscription/payments/liste.blade.php
@@ -4,35 +4,33 @@
         {{ $users->links() }} <!-- Afficher la pagination -->
     </div> --}}
 
-    <div class="container mx-auto p-28 mt-12 bg-gray-50 shadow-xl drop-shadow-sm">
+    <div class=" w-f mx-auto p-8 mt-12 bg-gray-50 shadow-xl drop-shadow-sm">
 
         <h2 class="mb-4">Accepted Transactions</h2>
         <table class="min-w-full">
             <thead>
                 <tr>
-                    <th class="text-left" >Transaction ID</th>
-                    <th class="text-left" >Amount</th>
-                    <th class="text-left" >Currency</th>
+                    <th class="text-left" >Nom</th>
+                    <th class="text-left" >Email</th>
+                    <th class="text-left" >Methode</th>
+                    <th class="text-left" >Montant</th>
+                    <th class="text-left" >Date abonnees</th>
+                    <th class="text-left" >Date Fin</th>
                     <th class="text-left" >Status</th>
-                    <th class="text-left" >Customer Email</th>
-                    <th class="text-left" >Operator ID</th>
-                    <th class="text-left" >Operator</th>
-                    <th class="text-left" >Paid Amount</th>
-                    <th class="text-left" >Payment Date</th>
+
                 </tr>
             </thead>
             <tbody>
                 @foreach ($payments as $payment)
                 <tr>
-                    <td>{{ $payment->transaction_id }}</td>
-                    <td>{{ $payment->amount }}</td>
-                    <td>{{ $payment->currency }}</td>
+                    <td>{{ $payment->user->name }}</td>
+                    <td>{{ $payment->user->email }}</td>
+                    <td>{{ $payment->subscriptionPlan->name }}</td>
+                    <td>{{ $payment->subscriptionPlan->price }}</td>
+                    <td>{{ $payment->created_at }}</td>
+                    <td>{{ $payment->end_date }}</td>
                     <td>{{ $payment->status }}</td>
-                    <td>{{ $payment->customer_email }}</td>
-                    <td>{{ $payment->operator_id }}</td>
-                    <td>{{ $payment->operator }}</td>
-                    <td>{{ $payment->paid_amount }} {{ $payment->paid_currency }}</td>
-                    <td>{{ $payment->payment_date }}</td>
+
                 </tr>
                 @endforeach
             </tbody>
