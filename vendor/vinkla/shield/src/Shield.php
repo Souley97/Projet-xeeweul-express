306e196b342d768893687d137ea046c30a1a6627
Souley97 <souleymane9700@gmail.com>
2024-07-16 16:35:50 +0000

    deployment

---
+++ vendor/vinkla/shield/src/Shield.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright (c) Vincent Klaiber.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/vinkla/laravel-shield
+ */
+
+declare(strict_types=1);
+
+namespace Vinkla\Shield;
+
+class Shield
+{
+    protected string $currentUser;
+
+    public function __construct(
+        protected array $users
+    ) {
+    }
+
+    public function verify(string|null $username, string|null $password, string|null $user = null): bool
+    {
+        if ($username === null || $password === null) {
+            return false;
+        }
+
+        $users = $this->getUsers($user);
+
+        foreach ($users as $user => $credentials) {
+            if (
+                password_verify($username, reset($credentials)) &&
+                password_verify($password, end($credentials))
+            ) {
+                $this->currentUser = $user;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function getUsers(string $user = null): array
+    {
+        if ($user !== null) {
+            return array_intersect_key($this->users, array_flip((array) $user));
+        }
+
+        return $this->users;
+    }
+
+    public function getCurrentUser(): string
+    {
+        return $this->currentUser;
+    }
+}
---
+++ vendor/vinkla/shield/src/ShieldMiddleware.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Copyright (c) Vincent Klaiber.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/vinkla/laravel-shield
+ */
+
+declare(strict_types=1);
+
+namespace Vinkla\Shield;
+
+use Closure;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class ShieldMiddleware
+{
+    public function __construct(
+        protected Shield $shield
+    ) {
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @throws \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
+     * @return mixed
+     */
+    public function handle($request, Closure $next, ?string $user = null): mixed
+    {
+        if ($this->shield->verify($request->getUser(), $request->getPassword(), $user) === false) {
+            throw new UnauthorizedHttpException('Basic');
+        }
+
+        return $next($request);
+    }
+}
